c0cf93eb2383543932903d1e64a0906c5ec65d2f
scarvajal <scarvajal@conversantmedia.com>
2019-12-23 13:35:04 -0500

    Activating / Obtaining / Validating Child Routes

--- src/app/products/product-edit/product-edit.component.ts
+++ src/app/products/product-edit/product-edit.component.ts
@@ -16,6 +16,8 @@ export class ProductEditComponent implements OnInit {
   errorMessage: string;
 
   product: Product;
+  private dataIsValid: { [key: string]: boolean} = {};
+
 
   constructor(private productService: ProductService,
               private messageService: MessageService,
@@ -90,5 +92,36 @@ export class ProductEditComponent implements OnInit {
         this.errorMessage = resolvedData.error;
         this.onProductRetrieved(resolvedData.product);
     });
+  }
+
+  validate(): void {
+    // Clear the validation object
+    this.dataIsValid = {};
+
+    // 'info' tab
+    if (this.product.productName &&
+          this.product.productName.length >= 3 &&
+          this.product.productCode) {
+          this.dataIsValid['info'] = true;
+    } else {
+      this.dataIsValid['info'] = false;
+    }
+
+    // 'tags' tab
+    if (this.product.category &&
+      this.product.category.length >= 3) {
+      this.dataIsValid['tags'] = true;
+    } else {
+      this.dataIsValid['tags'] = false;
+    }
+  }
+
+  isValid(path?: string): boolean {
+    this.validate();
+    if (path) {
+      return this.dataIsValid[path];
+    }
+    return (this.dataIsValid &&
+      Object.keys(this.dataIsValid).every(d => this.dataIsValid[d] === true));
   }
 }
