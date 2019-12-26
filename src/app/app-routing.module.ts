47e2a690e74c527d30a9c085d7bf0182f90d7d54
scarvajal <scarvajal@conversantmedia.com>
2019-12-26 10:34:02 -0500

    Custom Preloading Strategy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, PreloadAllModules } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { WelcomeComponent } from './home/welcome.component';
 import { PageNotFoundComponent } from './page-not-found.component';
 import { AuthGuard } from './user/auth.guard';
+import { SelectiveStrategy } from './selective-strategy.services';
 
 const ROUTES = [
   { path: 'welcome', component: WelcomeComponent },
   {
     path: 'products',
     canActivate: [AuthGuard],
+    data: { preload: false},
     loadChildren: () =>
         import('./products/product.module').then(m => m.ProductModule)
   },
@@ -18,7 +20,7 @@ const ROUTES = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(ROUTES, {enableTracing : true, preloadingStrategy : PreloadAllModules }),
+    RouterModule.forRoot(ROUTES, {enableTracing : true, preloadingStrategy : SelectiveStrategy }),
   ],
   exports: [ RouterModule ]
 })
